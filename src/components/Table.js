ef1a8e9b58705b2875461a777a14be649de3eb01
Joshua Salva <joshuarigs.salva@lc.cuny.edu>
2023-03-29 16:30:55 -0400

    recorded myself, showcasing the react project.

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,48 +1,5 @@
 import React from 'react'
 
-// const TableHeader = () => {
-//   // boilerplate table header functional component
-//   return (
-//     <thead>
-//       <tr>
-//         <th>Name</th>
-//         <th>URL</th>
-//         <th>Remove</th>
-//       </tr>
-//     </thead>
-//   )
-// }
-
-// const TableBody = ({linkData, removeLink}) => {
-//   // boilerplate table body functional component
-//   // we use Array.map to create table rows from LinkData passed via props
-//   const rows = linkData.map((row, index) => {
-//     return (
-//       <tr key={index}>
-//         <td>{row.name}</td>
-//         <td>
-//           <a href={row.URL}>{row.URL}</a>
-//         </td>
-//         <td>
-//           <button type="submit" onClick={() => removeLink(index)}>Delete</button>
-//         </td>
-//       </tr>
-//     )
-//   })
-
-//   return <tbody>{rows}</tbody>
-// }
-
-// const Table = (linkData, removeLink) => {
-//   {
-//     /*TODO - return <table> component, TableHeader and TableBody  and pass props!*/
-//     return <table>
-//       <TableHeader/>
-//       <TableBody linkData={linkData} removeLink={removeLink}/>
-//     </table>
-//   }
-// }
-
 const TableHeader = () => {
   // boilerplate table header functional component
   return (
